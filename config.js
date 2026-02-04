5a323c64ba8896a0381ca7a28c56a4ac257e2de0
tarikbellali2006-alt <tarikbellali2006@gmail.com>
2026-02-04 20:50:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "yeeees this muchhhh?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "i said moooooore! 🚀💝",              // Shows when they go past 1000%
+        normal: "nooo more"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
